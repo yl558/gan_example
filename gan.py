d6cccaea43d45e9ed92a2864f70be17b9e381ceb
Yang Liu <yl558@njit.edu>
2020-07-02 04:27:15 -0400

    Update gan.py

--- gan.py
+++ gan.py
@@ -185,20 +185,20 @@ def train(dataset, epochs):
     for epoch in range(epochs):
         start = time.time()
 
-    for image_batch in dataset:
-        train_step(image_batch)
+        for image_batch in dataset:
+            train_step(image_batch)
 
-    # Produce images for the GIF as we go
-    display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epoch + 1,
-                             seed)
+        # Produce images for the GIF as we go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                 epoch + 1,
+                                 seed)
 
-    # Save the model every 15 epochs
-    if (epoch + 1) % 15 == 0:
-        checkpoint.save(file_prefix = checkpoint_prefix)
+        # Save the model every 15 epochs
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
 
-    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
     # Generate after the final epoch
     display.clear_output(wait=True)
